367323a14ea14d54754ace0b2a8e6133c9607911
Nicholas Rahn <nick@transparentech.com>
2021-11-05 12:10:36 -0500

    Fix unit test for java 17

--- src/test/java/org/cujau/utils/ExceptionUtilTest.java
+++ src/test/java/org/cujau/utils/ExceptionUtilTest.java
@@ -1,6 +1,7 @@
 package org.cujau.utils;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 import java.lang.reflect.InvocationTargetException;
@@ -11,32 +12,32 @@ import org.slf4j.LoggerFactory;
 
 public class ExceptionUtilTest {
 
-    private static final Logger LOG = LoggerFactory.getLogger( ExceptionUtilTest.class );
+    private static final Logger LOG = LoggerFactory.getLogger(ExceptionUtilTest.class);
 
     @Test
     public void test1() {
         try {
-            ( (String) null ).length();
-        } catch ( Throwable t ) {
-            String trace = ExceptionUtil.getSimpletrace( t );
-            LOG.info( trace );
-            assertEquals( "NullPointerException", trace );
+            ((String) null).length();
+        } catch (Throwable t) {
+            String trace = ExceptionUtil.getSimpletrace(t);
+            LOG.info(trace);
+            assertTrue(trace.startsWith("NullPointerException"));
             return;
         }
-        fail( "Should not be here." );
+        fail("Should not be here.");
     }
 
     @Test
     public void test2() {
         try {
-            throw new RuntimeException( "Got an exception.",
-                                        new InvocationTargetException( new NullPointerException(),
-                                                                       "Problem Alert!" ) );
-        } catch ( Throwable t ) {
-            String trace = ExceptionUtil.getSimpletrace( t );
-            LOG.info( trace );
-            assertEquals( "RuntimeException : Got an exception.\n  InvocationTargetException : Problem Alert!\n    NullPointerException",
-                          trace );
+            throw new RuntimeException("Got an exception.",
+                                       new InvocationTargetException(new NullPointerException(), "Problem Alert!"));
+        } catch (Throwable t) {
+            String trace = ExceptionUtil.getSimpletrace(t);
+            LOG.info(trace);
+            assertEquals(
+                    "RuntimeException : Got an exception.\n  InvocationTargetException : Problem Alert!\n    NullPointerException",
+                    trace);
             return;
         }
     }
